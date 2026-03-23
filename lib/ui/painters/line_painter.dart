7425225e97f4afc85cb925de2d5c5ab77080389d
mnsh2011-ship-it <mnsh2011@gmail.com>
2026-03-23 23:56:45 +0300

    Create line_painter.dart file for rendering lines

---
+++ lib/ui/painters/line_painter.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LinePainter extends CustomPainter {
+  final List<Offset> points;
+
+  LinePainter(this.points);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 4.0;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      canvas.drawLine(points[i], points[i + 1], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
